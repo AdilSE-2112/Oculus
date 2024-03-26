6b9720ee6d72166a9724287fd90a6b1971bf1f27
AdilSE-2112 <97591283+AdilSE-2112@users.noreply.github.com>
2024-03-26 11:32:50 +0500

    login bag

--- src/components/AuthContext.jsx
+++ src/components/AuthContext.jsx
@@ -3,21 +3,25 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('access_token'));
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
     // Check authentication status (e.g., retrieve token from localStorage)
-    // const token = ;
-    // console.log(token)
-    // setIsAuthenticated(!!token);
+    const token = localStorage.getItem('access_token');
+    console.log('Token from localStorage:', token);
+    setIsAuthenticated(!!token);
   }, []);
 
   const login = () => {
     setIsAuthenticated(true);
+    // Save access_token to localStorage upon login
+    localStorage.setItem('access_token', 'your_access_token_here');
   };
 
   const logout = () => {
     setIsAuthenticated(false);
+    // Clear access_token from localStorage upon logout
+    localStorage.removeItem('access_token');
   };
 
   const value = {
